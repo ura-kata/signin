481c763f71333d299cf8fdb084f497e7ec90ae4b
uttne <uttne.mkr@gmail.com>
2021-03-15 02:22:30 +0900

    callback 画面で token を取得処理を追加

--- src/components/pages/CallbackPage.tsx
+++ src/components/pages/CallbackPage.tsx
@@ -1,32 +1,62 @@
-import { useLocation } from 'react-router';
-import { checkState } from '../../util';
+import { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router';
+import { getToken } from '../../util';
 import PageTemplate from '../templates/PageTemplate';
+
+interface Params {
+  code: string;
+  state: string;
+}
+
+const getCodeAndState = (locationSearch: string): Params | undefined => {
+  const params = locationSearch;
+  if (params === undefined) return undefined;
+  if (params === '') return undefined;
+  const list = params.split(/[?]|[&]/);
+
+  let code: string | undefined = undefined;
+  let state: string | undefined = undefined;
+
+  list.forEach((l) => {
+    if (!code && l.startsWith('code=')) {
+      code = l.substring(5);
+    } else if (!state && l.startsWith('state=')) {
+      state = l.substring(6);
+    }
+  });
+
+  if (code && state) {
+    return {
+      code: code,
+      state: state,
+    };
+  }
+  return undefined;
+};
 
 export default function CallbackPage() {
   const location = useLocation();
-  const getCodeAndState = (): { code: string; state: string } | undefined => {
-    const params = location.search;
-    const list = params.split(/[?]|[&]/);
 
-    let code: string | undefined = undefined;
-    let state: string | undefined = undefined;
-
-    list.forEach((l) => {
-      if (!code && l.startsWith('code=')) {
-        code = l.substring(5);
-      } else if (!state && l.startsWith('state=')) {
-        state = l.substring(6);
-      }
-    });
-
-    if (code && state && checkState(state)) {
-      return {
-        code: code,
-        state: state,
-      };
+  useEffect(() => {
+    if (!location) return;
+    const params = getCodeAndState(location.search);
+    if (!params) {
+      window.location.href = '/signin';
+      return;
     }
-    return undefined;
-  };
+
+    const f = async () => {
+      const token = await getToken(params.code, params.state);
+      if (!token) {
+        window.location.href = '/signin';
+        return;
+      }
+      console.log(token?.access_token);
+
+      // TODO Cookie に Token を設定しアプリケーションに遷移する
+    };
+    f();
+  }, [location]);
 
   return (
     <PageTemplate>
